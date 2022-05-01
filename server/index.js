285eb3c6ec82fb8e3895d15d8bacc21e7b121cda
Danial <danialzafarfrpf@gmail.com>
2022-05-01 11:42:24 +0500

    Sending submodules that weren't send in last update

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
+//Importing main  dependencies
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+//Initializing express app instance
+const app = express();
+
+//Setting up body-parser to properly send request
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+//Setting cors for usage
+app.use(cors());
