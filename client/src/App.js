68e984ac818d66ed9dd41e0c3ab90b949478b6e3
Danial <danialzafarfrpf@gmail.com>
2022-05-04 08:45:01 +0500

    Started working on Pagination and added functionality to redirect logged in users back to home page if they go to login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 //Importing material components
 import { Container } from "@material-ui/core";
@@ -8,26 +8,42 @@ import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar.js";
 import Home from "./components/Home/Home.js";
 import Auth from "./components/Auth/Auth.js";
+import PostDetails from "./components/PostDetails/PostDetails.js";
 
 const App = () => {
-  return (
-    //Container to center everything
-    //Typography in material stand for any textual element but it gives us a nice looking font
-    //Grow provides some simple animations
-    //Grid contains a container and its item
-    //Each item has 12 columns which can be specified for different devices
-    //xs is very small and sm is small devices
+    const user = JSON.parse(localStorage.getItem("profile"));
 
-    <BrowserRouter>
-      <Container maxWidth="lg">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
-        </Switch>
-      </Container>
-    </BrowserRouter>
-  );
+    return (
+        //Container to center everything
+        //Typography in material stand for any textual element but it gives us a nice looking font
+        //Grow provides some simple animations
+        //Grid contains a container and its item
+        //Each item has 12 columns which can be specified for different devices
+        //xs is very small and sm is small devices
+
+        <BrowserRouter>
+            <Container maxWidth="xl">
+                <Navbar />
+                <Switch>
+                    <Route
+                        path="/"
+                        exact
+                        component={() => <Redirect to="/posts" />}
+                    />
+                    <Route path="/posts" exact component={Home} />
+                    <Route path="/posts/search" exact component={Home} />
+                    <Route path="/posts/:id" component={PostDetails} />
+                    <Route
+                        path="/auth"
+                        exact
+                        component={() =>
+                            !user ? <Auth /> : <Redirect to="/posts" />
+                        }
+                    />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    );
 };
 
 export default App;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 //Adding material components
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 
 //Importing hook from redux
 import { useDispatch } from "react-redux";
@@ -10,44 +10,51 @@ import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts.js";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
+import Pagination from "../Pagination.jsx";
 
 //Adding styles
 import useStyles from "./styles.js";
 
 const Home = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  //Initializing id state
-  const [currentId, setCurrentId] = useState(null);
+    //Initializing id state
+    const [currentId, setCurrentId] = useState(null);
 
-  //Defining dispatch
-  const dispatch = useDispatch();
+    //Defining dispatch
+    const dispatch = useDispatch();
 
-  //Here getPosts is the action
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
+    //Here getPosts is the action
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
-  return (
-    <Grow in>
-      <Container>
-        <Grid
-          className={classes.mainContainer}
-          container
-          justifyContent="space-between"
-          alignItems="stretch"
-          spacing={3}
-        >
-          <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId} />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
-          </Grid>
-        </Grid>
-      </Container>
-    </Grow>
-  );
+    return (
+        <Grow in>
+            <Container>
+                <Grid
+                    className={classes.mainContainer}
+                    container
+                    justifyContent="space-between"
+                    alignItems="stretch"
+                    spacing={3}
+                >
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form
+                            currentId={currentId}
+                            setCurrentId={setCurrentId}
+                        />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    );
 };
 
 export default Home;
